03c29bea0c53eab1d5b0665167ad4d7d12d83dd8
nephtys <christopher.kaag@gmail.com>
2017-10-21 10:40:55 +0200

    fixed sorting issue and added possibility to use case insensitive kd trees

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 
 use std::collections::HashMap;
-
+use std::cmp::Ordering;
 
 #[cfg(test)]
 mod tests {
@@ -33,13 +33,20 @@ mod tests {
 
     #[test]
     fn mock_works() {
-        let tree = MockKDTree::build(&vec![MyEntity{
-            id: 1, txt: "Apple".to_string()
-        }, MyEntity{
-            id: 2, txt: "Banana".to_string()
-        }, MyEntity{
-            id: 3, txt: "Lettuce".to_string()
-        }]);
+        let tree = MockKDTree::build(&vec![
+            MyEntity {
+                id: 1,
+                txt: "Apple".to_string(),
+            },
+            MyEntity {
+                id: 2,
+                txt: "Banana".to_string(),
+            },
+            MyEntity {
+                id: 3,
+                txt: "Lettuce".to_string(),
+            },
+        ], true);
 
         let first_result = tree.search("an");
         assert_eq!(first_result.len(), 1);
@@ -53,6 +60,41 @@ mod tests {
         assert_eq!(second_result[1].id, 3);
         assert_eq!(second_result[1].index, 1);
     }
+
+
+    #[test]
+    fn mock_case_insensitive_works() {
+        let tree = MockKDTree::build(&vec![
+            MyEntity {
+                id: 1,
+                txt: "Apple".to_string(),
+            },
+            MyEntity {
+                id: 2,
+                txt: "Banana".to_string(),
+            },
+            MyEntity {
+                id: 3,
+                txt: "Lettuce".to_string(),
+            },
+        ], false);
+
+        let first_result = tree.search("A");
+        assert_eq!(first_result.len(), 2);
+        println!("{:?}", first_result);
+        assert_eq!(first_result[0].id, 1);
+        assert_eq!(first_result[0].index, 0);
+        assert_eq!(first_result[1].id, 2);
+        assert_eq!(first_result[1].index, 1);
+
+        let second_result = tree.search("a");
+        assert_eq!(second_result.len(), 2);
+        println!("{:?}", first_result);
+        assert_eq!(second_result[0].id, 1);
+        assert_eq!(second_result[0].index, 0);
+        assert_eq!(second_result[1].id, 2);
+        assert_eq!(second_result[1].index, 1);
+    }
 }
 
 
@@ -61,37 +103,68 @@ pub trait SearchableElement {
     fn get_id(&self) -> u32;
 }
 
+#[derive(Debug, Eq, PartialEq, PartialOrd)]
 pub struct SearchResult {
     id: u32,
     index: usize,
 }
 
-pub trait KDTree {
-    fn build<T: SearchableElement>(elements : &Vec<T>) -> Self;
-    fn search(&self, query: &str) -> Vec<SearchResult>;
+
+impl std::cmp::Ord for SearchResult {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let idcmp = self.id.cmp(&other.id);
+        if idcmp == Ordering::Equal {
+            return self.index.cmp(&other.index);
+        } else {
+            return idcmp;
+        }
+    }
 }
 
+
+
+pub trait KDTree {
+    fn build<T: SearchableElement>(elements: &Vec<T>, case_sensitive: bool) -> Self;
+    fn search(&self, query: &str) -> Vec<SearchResult>;
+    fn is_case_sensitive(&self) -> bool;
+}
+
+#[derive(Debug)]
 pub struct MockKDTree {
-    elements : HashMap<u32, String>,
+    elements: HashMap<u32, String>,
+    case_sensitive: bool,
 }
 
 impl KDTree for MockKDTree {
-    fn build<T: SearchableElement>(elements: &Vec<T>) -> Self {
+    fn build<T: SearchableElement>(elements: &Vec<T>, case_sensitive: bool) -> Self {
         let mut tree = MockKDTree {
-            elements: HashMap::new()
+            elements: HashMap::new(),
+            case_sensitive: case_sensitive,
         };
         for element in elements {
-            tree.elements.insert(element.get_id(), element.as_searchable_text());
+            tree.elements.insert(
+                element.get_id(),
+                if case_sensitive {
+                    element.as_searchable_text()
+                } else {
+                    element.as_searchable_text().to_lowercase()
+                },
+            );
         }
         return tree;
     }
 
     fn search(&self, query: &str) -> Vec<SearchResult> {
-        let mut results : Vec<SearchResult> = Vec::new();
+        let mut results: Vec<SearchResult> = Vec::new();
+
+        let query = if self.is_case_sensitive() {
+            query.to_string()
+        } else {
+            query.to_lowercase()
+        };
 
         for (id, string) in &self.elements {
-
-            let index_opt = string.find(query);
+            let index_opt = string.find(&query);
 
             if let Some(idx) = index_opt {
                 results.push(SearchResult {
@@ -100,7 +173,10 @@ impl KDTree for MockKDTree {
                 });
             }
         }
-
+        results.sort();
         return results;
+    }
+    fn is_case_sensitive(&self) -> bool {
+        return self.case_sensitive;
     }
 }
